d562d24180a182f4b7713cd2a1090579097ed97c
Omais javeed <100470778+omaisjaveed@users.noreply.github.com>
2026-04-14 23:30:13 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ $id = $_GET['id'];
 //deleting the row from table
 $result = mysqli_query($conn, "DELETE FROM employee WHERE id=$id");
 
-//redirecting to the display page for smooth direction (index.php in our case)
+//redirecting to the display page for smooth direction -- (index.php in our case)
 header("Location:viewemp.php");
 ?>
 
