368257f176aa713b459a56accce9a01068a1b275
Omais javeed <100470778+omaisjaveed@users.noreply.github.com>
2026-04-24 03:44:10 +0500

    Update assign.php

--- assign.php
+++ assign.php
@@ -13,11 +13,11 @@
     <!-- Font special for pages-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 
-    <!-- Vendor CSS-->
+    <!-- Vendor CSS File-->
     <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
     <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
 
-    <!-- Main CSS-->
+    <!-- Main CSS File-->
     <link href="css/main.css" rel="stylesheet" media="all">
 </head>
 
